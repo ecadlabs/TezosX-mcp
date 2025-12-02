c51f317c1498aea227e3684430cd9c4ea4b91c9e
Maxwell Ward <max.jp.ward@gmail.com>
2025-12-02 12:36:27 -0800

    move frontend inside mcp

--- mcp/src/webserver.ts
+++ mcp/src/webserver.ts
@@ -6,7 +6,7 @@ import sirv from "sirv";
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export const startWebServer = (port: number) => {
-	const distPath = join(__dirname, "../../frontend/dist");
+	const distPath = join(__dirname, "../frontend/dist");
 	const serve = sirv(distPath, { single: true });
 	createServer(serve).listen(port);
 };
